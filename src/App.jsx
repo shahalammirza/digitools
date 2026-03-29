6b00a7a6a084a3ceee98d44f213ae2aba733b6fb
shahalammirza <mshahalam1246@gmail.comm>
2026-03-30 00:49:19 +0600

    Initial project setup with vite, tailwind css, daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+
+function App() {
+  
+  return (
+    <>
+    <NavBar></NavBar>
+      <h2 className='text-5xl text-center font-bold p-40 bg-amber-500 text-white'>Hello Bangladesh</h2>
+    </>
+  )
+}
+
+export default App
